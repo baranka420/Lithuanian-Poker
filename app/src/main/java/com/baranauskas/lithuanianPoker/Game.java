3c6af4ec11f7515da886b4321a374c0d1faa7cc6
baranka420 <juliu100@gmail.com>
2017-11-15 20:41:46 +0200

    vaizduoja kortas, pradedu daryt eiles keitimo funkcijas ir palyginima

--- app/src/main/java/com/baranauskas/lithuanianPoker/Game.java
+++ app/src/main/java/com/baranauskas/lithuanianPoker/Game.java
@@ -6,7 +6,6 @@ package com.baranauskas.lithuanianPoker;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -18,15 +17,17 @@ import android.widget.TextView;
 public class Game extends AppCompatActivity {
     Button takeTurnButton;
     Spinner pickCombinationSpinner;
-    TextView firstCard, nameDisplay;
+    TextView firstCard, secondCard, thirdCard, fourthCard, nameDisplay;
     ImageView iv1, iv2, iv3, iv4;
-    private ImageView[] images = {iv1, iv2, iv3, iv4};
+    Drawable drawale;
+    private ImageView[] images = new ImageView[4];
+    private TextView[] cardTexts = new TextView[4];
     final int minSuitsId = 0;
     final int maxSuitsId = 3;
     final int minId = 9;
     final int maxId = 14;
     int playerCount;
-    int cardCount = 1;
+    int cardCount = 4;
     int playerTurn = 0;
     String playerNames;
     Card[] cards = new Card[cardCount];
@@ -42,22 +43,29 @@ public class Game extends AppCompatActivity {
         takeTurnButton = (Button) findViewById(R.id.takeTurnButton);
         pickCombinationSpinner = (Spinner) findViewById(R.id.pickCombinationSpinner);
         firstCard = (TextView) findViewById(R.id.firstCard);
+        secondCard = (TextView) findViewById(R.id.secondCard);
+        thirdCard = (TextView) findViewById(R.id.thirdCard);
+        fourthCard = (TextView) findViewById(R.id.fourthCard);
         nameDisplay = (TextView) findViewById(R.id.nameDisplay);
         iv1 = (ImageView) findViewById(R.id.iv1);
         iv2 = (ImageView) findViewById(R.id.iv2);
         iv3 = (ImageView) findViewById(R.id.iv3);
         iv4 = (ImageView) findViewById(R.id.iv4);
-        Context context = this.getApplicationContext();
+        images[0] = iv1;
+        images[1] = iv2;
+        images[2] = iv3;
+        images[3] = iv4;
+        cardTexts[0] = firstCard;
+        cardTexts[1] = secondCard;
+        cardTexts[2] = thirdCard;
+        cardTexts[3] = fourthCard;
         Intent gameDataIntent = getIntent();
         playerCount = gameDataIntent.getExtras().getInt("playerCount");
-        //Intent playerNamesIntent = getIntent();
         playerNames = gameDataIntent.getExtras().getString("playerNames");
         playerNamesArray = playerNames.split(",");
         allCards = new AllCards[24];
         players = new Player[playerCount];
         for(int x = 0; x < cardCount; x++){
-            //int cardId = minId + (int)(Math.random() * ((maxId - minId) + 1));
-            //int cardSuitsId = minSuitsId + (int)(Math.random() * ((maxSuitsId - minSuitsId) + 1));
             cards[x] = new Card(minId, giveSuitsNameById(minSuitsId));
         }
         int i = 0;
@@ -71,7 +79,7 @@ public class Game extends AppCompatActivity {
             players[x] = new Player(playerNamesArray[x], cards, cardCount);
             dealCards(x);
         }
-        showPlayerView(playerTurn, context);
+        showPlayerView(playerTurn);
     }
 
     public void dealCards(int playerNumber){
@@ -81,8 +89,10 @@ public class Game extends AppCompatActivity {
             for(int x = 0; x < allCards.length; x++){
                 if(allCards[x].cardId == players[playerNumber].playerCards[y].cardNameID && allCards[x].cardSuit == players[playerNumber].playerCards[y].cardSuit && allCards[x].occupied()){
                     y-=1;
+                    break;
                 }
             }
+            allCards[y].occupied = true;
         }
     }
 
@@ -108,18 +118,24 @@ public class Game extends AppCompatActivity {
         return suitsId;
     }
 
-    public void showPlayerView(int playerTurn, Context ctx){
-        firstCard.setText(players[playerTurn].playerCards[0].cardSuit + players[playerTurn].playerCards[0].cardNameID);
+    public void showPlayerView(int playerTurn){
         nameDisplay.setText(players[playerTurn].getPlayerName());
         for(int x = 0; x < players[playerTurn].cardCount; x++){
             //String a = "drawable://" + "a" + Integer.toString(players[playerTurn].playerCards[x].cardNameID) + "_of_" + players[playerTurn].playerCards[x].cardSuit;
 
             String name = "a" + Integer.toString(players[playerTurn].playerCards[x].cardNameID) + "_of_" + players[playerTurn].playerCards[x].cardSuit;
             int id = getResources().getIdentifier(name, "drawable", getPackageName());
-            Drawable drawale = getResources().getDrawable(id);
-            //images[x].setImageDrawable(ContextCompat.getDrawable(ctx , id));
+            drawale = getResources().getDrawable(id);
             images[x].setImageDrawable(drawale);
-            //images[x].setImageDrawable(drawale);
+            cardTexts[x].setText(players[playerTurn].playerCards[x].cardSuit + players[playerTurn].playerCards[x].cardNameID);
+
+        }
+    }
+    public void setNextTurn(int currentTurn, String currentCombination){
+        if(currentTurn + 1 > playerCount - 1){
+            playerTurn = 0;
+        }else{
+            playerTurn++;
         }
     }
 }
